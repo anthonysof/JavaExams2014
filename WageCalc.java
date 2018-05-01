d9b49fe6173fc28cd6df1c1d515a9fed4919b608
anthonysof <antonis.sofras@gmail.com>
2018-05-01 23:56:24 +0300

    index if clause, outter object to call inner class cstor

--- WageCalc.java
+++ WageCalc.java
@@ -1,94 +1,105 @@
+
 /**
-	Themata Java 2014
-	DO NOT ATTEMPT TO IMPORT TO ECLIPSE
-	Kyriakos Giannakis
-*/
+ 	Themata Java 2014
+ 	DO NOT ATTEMPT TO IMPORT TO ECLIPSE
+ 	Kyriakos Giannakis
+ */
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class WageCalc{
-	private static RandomAccessFile f_emp;
-	private static RandomAccessFile f_data;
+    private static RandomAccessFile f_emp;
+    private static RandomAccessFile f_data;
 
-	// List with all the employees.
-	private List<Employee> employees = new ArrayList<Employee>();
-
-
-	private static String readString(int length, RandomAccessFile rafGiven) throws IOException{
-		byte b[] = new byte[length];
-		rafGiven.readFully(b);
-		return new String(b);
-	}
+    // List with all the employees.
+    private static List<Employee> employees = new ArrayList<Employee>();
 
 
-	/**
-		Represents an employee and his details.
-	*/
-	private class Employee{
+    private static String readString(int length, RandomAccessFile rafGiven) throws IOException {
+        byte b[] = new byte[length];
+        rafGiven.readFully(b);
+        return new String(b);
+    }
 
-		private String name, address, tel;
-		private int property, children;
-		private double wage;
 
-		// Getters, setters... Do them urself :P
-	}
+    /*
+        Represents an employee and his details.
+    */
+    private static class Employee{
 
-	public static void main(String[] args) throws IOException{
-		
-		f_emp = new RandomAccessFile("employee.dbs","rw");
-		f_data = new RandomAccessFile("data.dbs","rw");
+        private String name, address, tel;
+        private int property, children;
+        private double wage;
 
-		int index = 1;
-		try{
-			while(true){
-				// Move the cursor to the employee record,
-				// both on the employee file and data file.
-				f_emp.seek((index*140) - 1);
-				f_data.seek((index*16) - 1);
+        // Getters, setters... Do them urself :P
+    }
 
-				Employee enew = new Employee();
+    public static void main(String[] args) throws IOException{
 
-				// Read Employee.dbs and extract the data to the employee object
+        f_emp = new RandomAccessFile("employee.dbs","rw");
+        f_data = new RandomAccessFile("data.dbs","rw");
 
-				enew.setName(readString(60, f_emp));  // Name
-				enew.setAddress(readString(50, f_emp));  // Address
-				enew.setTel(readString(30, f_emp));
+        int index = 0;
+        try{
+            while(true){
+                // Move the cursor to the employee record,
+                // both on the employee file and data file.
+                if(index == 0)
+                {
+                    f_emp.seek(140 - 1);
+                    f_data.seek(16 - 1);
+                }
+                else {
+                    f_emp.seek((index * 140) - 1);
+                    f_data.seek((index * 16) - 1);
+                }
+                Employee enew = new Employee();
 
-				// Read Data.dbs...
+                // Read Employee.dbs and extract the data to the employee object
 
-				enew.setProperty(f_data.readInt());  // Property
-				enew.setChildren(f_data.readInt());  // Children
-				enew.setWage(f_data.readDouble());  // Wage
+                enew.setName(readString(60, f_emp));  // Name
+                enew.setAddress(readString(50, f_emp));  // Address
+                enew.setTel(readString(30, f_emp));
 
-				employees.add(enew);
-				index++;
-			}
-		} catch(EOFException e){}
+                // Read Data.dbs...
 
-		Scanner s = new Scanner(System.in);
-		System.out.println("Please give me K1: ");
-		int k1 = s.nextInt();
-		System.out.println("Please give me K2: ");
-		int k2 = s.nextInt();
+                enew.setProperty(f_data.readInt());  // Property
+                enew.setChildren(f_data.readInt());  // Children
+                enew.setWage(f_data.readDouble());  // Wage
 
-		// Too bored to check for invalid input
+                employees.add(enew);
+                index++;
 
-		try{
-			FileWriter fw = new FileWriter(new File("raise.txt"));
-			for (int i = k1 - 1; i < k2 - 1; i++){
-				Employee e = employees.get(i);
-				if (e.getChildren() == 2 && e.getWage() < 2000.0 && e.getProperty() == 0){
-					fw.write(String.valueOf(i +1) + ",");  // Employee ID
-					fw.write(e.getName() + ",");  // Name
-					fw.write(String.valueOf(e.getChildren() + ","));  // Children
-					fw.write(String.valueOf(e.getWage() + (0.1 * e.getWage())));  // New Wage (wage + wage*0.1)
-					fw.write(System.lineSeparator());
-				}
-			}
-			fw.close();
-		} catch (IOException e){}
-		
-		
+        } catch(EOFException e){}
 
-		
-	}
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please give me K1: ");
+        int k1 = s.nextInt();
+        System.out.println("Please give me K2: ");
+        int k2 = s.nextInt();
+
+        // Too bored to check for invalid input
+
+        try{
+            FileWriter fw = new FileWriter(new File("raise.txt"));
+            for (int i = k1 - 1; i < k2 - 1; i++){
+                Employee e = employees.get(i);
+                if (e.getChildren() == 2 && e.getWage() < 2000.0 && e.getProperty() == 0){
+                    fw.write(String.valueOf(i +1) + ",");  // Employee ID
+                    fw.write(e.getName() + ",");  // Name
+                    fw.write(String.valueOf(e.getChildren() + ","));  // Children
+                    fw.write(String.valueOf(e.getWage() + (0.1 * e.getWage())));  // New Wage (wage + wage*0.1)
+                    fw.write(System.lineSeparator());
+                }
+            }
+            fw.close();
+        } catch (IOException e){}
+
+
+
+
+    }
 }
-
